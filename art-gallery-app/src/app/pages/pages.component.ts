3791b1e4a6d33a48727d6bbaa66709ebc9b37f9e
rtyl <rtyl@gwl.com>
2019-05-28 09:42:28 -0600

    removed animation and better aligned template.

--- art-gallery-app/src/app/pages/pages.component.ts
+++ art-gallery-app/src/app/pages/pages.component.ts
@@ -15,40 +15,13 @@ var states = new States();
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
-  styleUrls: ['./pages.component.scss'],
-  animations: [
-    trigger('mainState', [
-      state(states.inactive, style({
-        padding: styling.header.height+' 0 0 0'
-      })),
-      state(states.active,   style({
-        padding: styling.header.height+' 0 0 '+styling.sidebar.width
-      })),
-      transition(states.inactive+' => '+states.active, animate('500ms ease-in')),
-      transition(states.active+' => '+states.inactive, animate('500ms ease-out'))
-    ]),
-    trigger('navbarState', [
-      state(states.inactive, style({
-        left: '-'+styling.sidebar.width
-      })),
-      state(states.active,   style({
-        left: '0'
-      })),
-      transition(states.inactive+' => '+states.active, animate('500ms ease-in')),
-      transition(states.active+' => '+states.inactive, animate('500ms ease-out'))
-    ])
-  ]
+  styleUrls: ['./pages.component.scss']
 })
 export class PagesComponent implements OnInit {
-  private menuState = states.active;
 
   constructor() { }
 
   ngOnInit() {
-  }
-
-  updateMenu(state: string) {
-    this.menuState = state;
   }
 
 }
